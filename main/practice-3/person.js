6a5299e36558fc83d276b57d05072e1b79e465cc
313183373 <313183373@qq.com>
2017-08-19 13:52:55 +0800

    prototype

--- main/practice-3/person.js
+++ main/practice-3/person.js
@@ -1,17 +1,16 @@
 // Write your code here
-class Person {
-    constructor(name,age){
-        this.name=name;
-        this.age=age;
-        this.id=Person.idcount++;
-    }
+function Person(name, age) {
+    this.name=name;
+    this.age=age;
+    this.id=Person.prototype.ids++;
+}
 
-    introduce(){
-        return this.age===1?`My name is ${this.name}. I am ${this.age} year old`:`My name is ${this.name}. I am ${this.age} years old.`;
-    }
-
+Person.prototype.introduce=function () {
+    if(this.age===1) return `My name is ${this.name}. I am ${this.age} year old.`;
+    return `My name is ${this.name}. I am ${this.age} years old.`;
 };
 
-Person.idcount=1;
+Person.prototype.ids=1;
 
 module.exports =Person;
+
